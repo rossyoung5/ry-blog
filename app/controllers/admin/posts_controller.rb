b73e3509db355bb0ff79465d48606547001d9e23
Ross Young <ross.young12@gmail.com>
2016-06-14 16:04:03 -0500

    removes namespacing for posts and adds conditionals for admin usage

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,80 +1,162 @@
-class Admin::PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+# class PostsController < ApplicationController
+#   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
-  # GET /posts.json
-  def index
-    if params[:category_id]
-      @category=Category.find(params[:category_id])
-      @posts=Post.order(date: :desc).where(category_id: @category.id).paginate(:page => params[:page])
-    else
-      @posts = Post.order(date: :desc).paginate(:page => params[:page])
-    end
-  end
+#   # GET /posts
+#   # GET /posts.json
+#   def index
+#     if params[:category_id]
+#       @category=Category.find(params[:category_id])
+#       @posts=Post.order(date: :desc).where(category_id: @category.id).paginate(:page => params[:page])
+#     else
+#       @posts = Post.order(date: :desc).paginate(:page => params[:page])
+#     end
+#   end
 
-  # GET /posts/1
-  # GET /posts/1.json
-  def show
-  end
+#   # GET /posts/1
+#   # GET /posts/1.json
+#   def show
+#   end
 
-  # GET /posts/new
-  def new
-    @post = Post.new
-  end
+#   # GET /posts/new
+#   def new
+#     @post = Post.new
+#   end
 
-  # GET /posts/1/edit
-  def edit
-  end
+#   # GET /posts/1/edit
+#   def edit
+#   end
 
-  # POST /posts
-  # POST /posts.json
-  def create
-    @post = Post.new(post_params)
+#   # POST /posts
+#   # POST /posts.json
+#   def create
+#     @post = Post.new(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to admin_post_url(@post), notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+#     respond_to do |format|
+#       if @post.save
+#         format.html { redirect_to @post, notice: 'Post was successfully created.' }
+#         format.json { render :show, status: :created, location: @post }
+#       else
+#         format.html { render :new }
+#         format.json { render json: @post.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to admin_post_url(@post), notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+#   # PATCH/PUT /posts/1
+#   # PATCH/PUT /posts/1.json
+#   def update
+#     respond_to do |format|
+#       if @post.update(post_params)
+#         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+#         format.json { render :show, status: :ok, location: @post }
+#       else
+#         format.html { render :edit }
+#         format.json { render json: @post.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
-  def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to admin_posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+#   # DELETE /posts/1
+#   # DELETE /posts/1.json
+# def destroy
+#   @post.destroy
+#   respond_to do |format|
+#     format.html { redirect_to admin_posts_url, notice: 'Post was successfully destroyed.' }
+#     format.json { head :no_content }
+#   end
+# end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+# private
+#   # Use callbacks to share common setup or constraints between actions.
+#   def set_post
+#     @post = Post.find(params[:id])
+#   end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:title, :date, :body, :category_id, :avatar)
-    end
-end
+#   # Never trust parameters from the scary internet, only allow the white list through.
+#   def post_params
+#     params.require(:post).permit(:title, :date, :body, :category_id, :avatar)
+#   end
+# end
+
+# namespaced version below
+
+# class Admin::PostsController < ApplicationController
+#   before_action :set_post, only: [:show, :edit, :update, :destroy]
+#   before_action :authenticate_user!
+
+#   # GET /posts
+#   # GET /posts.json
+#   def index
+#     if params[:category_id]
+#       @category=Category.find(params[:category_id])
+#       @posts=Post.order(date: :desc).where(category_id: @category.id).paginate(:page => params[:page])
+#     else
+#       @posts = Post.order(date: :desc).paginate(:page => params[:page])
+#     end
+#   end
+
+#   # GET /posts/1
+#   # GET /posts/1.json
+#   def show
+#   end
+
+#   # GET /posts/new
+#   def new
+#     @post = Post.new
+#   end
+
+#   # GET /posts/1/edit
+#   def edit
+#   end
+
+#   # POST /posts
+#   # POST /posts.json
+#   def create
+#     @post = Post.new(post_params)
+
+#     respond_to do |format|
+#       if @post.save
+#         format.html { redirect_to admin_post_url(@post), notice: 'Post was successfully created.' }
+#         format.json { render :show, status: :created, location: @post }
+#       else
+#         format.html { render :new }
+#         format.json { render json: @post.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
+
+#   # PATCH/PUT /posts/1
+#   # PATCH/PUT /posts/1.json
+#   def update
+#     respond_to do |format|
+#       if @post.update(post_params)
+#         format.html { redirect_to admin_post_url(@post), notice: 'Post was successfully updated.' }
+#         format.json { render :show, status: :ok, location: @post }
+#       else
+#         format.html { render :edit }
+#         format.json { render json: @post.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
+
+#   # DELETE /posts/1
+#   # DELETE /posts/1.json
+#   def destroy
+#     @post.destroy
+#     respond_to do |format|
+#       format.html { redirect_to admin_posts_url, notice: 'Post was successfully destroyed.' }
+#       format.json { head :no_content }
+#     end
+#   end
+
+#   private
+#     # Use callbacks to share common setup or constraints between actions.
+#     def set_post
+#       @post = Post.find(params[:id])
+#     end
+
+#     # Never trust parameters from the scary internet, only allow the white list through.
+#     def post_params
+#       params.require(:post).permit(:title, :date, :body, :category_id, :avatar)
+#     end
+# end
